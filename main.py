b82c4d9562af7716c3b88155e79adc5a2e908edc
Svensk2137 <kubadmidm@wp.pl>
2026-03-23 09:38:35 +0100

    sex

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ load_dotenv() # DISCORD_TOKEN
 
 client = discord.Bot()
 
+@client.event
+async def on_ready():
+	print(f"Logged in as {client.user}")
 
+@client.slash_command(name="hello", description="Say hello")
+async def hello(ctx: discord.ApplicationContext):
+	await ctx.respond("Hello")
+
+client.run(os.getenv("DISCORD_TOKEN"))
