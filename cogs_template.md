f618bdb15ff908bc626254631997fa4ee5b4a60f
Svensk2137 <kubadmidm@wp.pl>
2026-03-23 17:22:54 +0100

    cum

--- cogs_template.md
+++ cogs_template.md
@@ -2,22 +2,18 @@ Use this for adding cogs brother
 
 ```
 import discord
+from discord.ext import commands
 
 
-class Greetings(commands.Cog):  # create a class for our cog that inherits from commands.Cog
-    # this class is used to create a cog, which is a module that can be added to the bot
-
-    def __init__(self, bot):  # this is a special method that is called when the cog is loaded
+class Example(commands.Cog):
+    def __init__(self, bot):
         self.bot = bot
 
-    @discord.slash_command()  # we can also add application commands
-    async def goodbye(self, ctx):
-        await ctx.respond('Goodbye!')
+    @commands.slash_command()
+    async def hello(self, ctx: discord.ApplicationContext):
+        await ctx.respond("Hi, this is a slash command from a cog!")
 
-    @discord.user_command()
-    async def greet(self, ctx, member: discord.Member):
-        await ctx.respond(f'{ctx.author.mention} says hello to {member.mention}!')
 
-def setup(bot):  # this is called by Pycord to setup the cog
-    bot.add_cog(Greetings(bot))  # add the cog to the bot
+def setup(bot):
+    bot.add_cog(Example(bot))
 ```
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from dotenv import load_dotenv
 import discord
+from discord import application_command
 import os
 
 load_dotenv() # DISCORD_TOKEN
+DEBUG_GUILDS = [os.getenv("DEBUG_GUILDS")]
 
-client = discord.Bot()
+client = discord.Bot(debug_guilds=DEBUG_GUILDS)
 
-client.load_extension("cogs.greetings")
+
+for file in os.listdir("cogs"):
+	if file.endswith(".py"):
+		client.load_extension(f"cogs.{file[:-3]}")
 
 @client.event
 async def on_ready():
@@ -15,5 +20,21 @@ async def on_ready():
 @client.slash_command(name="ping", description="Latency")
 async def ping(ctx: discord.ApplicationContext):
 	await ctx.respond(f"Latency is {client.latency}")
+
+@client.slash_command()
+async def reload_cogs(ctx: discord.ApplicationContext):
+	message = "Reloading cogs...\n"
+	msg = await ctx.respond(f"```{message}```")
+	for file in os.listdir("cogs"):
+		if file.endswith(".py"):
+			try:
+				client.reload_extension(f"cogs.{file[:-3]}")
+				message += f"Reload cogs.{file[:-3]}    Success\n"
+				await msg.edit(content=f"```{message}```")
+			except:
+				message += f"Reload cogs.{file[:-3]}    Fail\n"
+				await msg.edit(content=f"```{message}```")
+	message += "Finished"
+	await msg.edit(content=f"```{message}```")
 
 client.run(os.getenv("DISCORD_TOKEN"))
