ce2e911a53178e73d45d0745e93ba61bbf740318
Svensk2137 <kubadmidm@wp.pl>
2026-03-24 19:28:58 +0100

    yea

--- error_handler.py
+++ error_handler.py
@@ -9,16 +9,14 @@ class Error_handler(commands.Cog):
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.respond("Command on cooldown.")
+            await ctx.respond("Command on cooldown.", delete_after=5)
         elif isinstance(error, commands.NotOwner):
-            await ctx.respond("Owner only command")
-        elif isinstance(error, commands.MissingPermissions):
-            await ctx.respond(error)
+            await ctx.respond("Owner only command", delete_after=5)
         elif isinstance(error, discord.Forbidden):
-            await ctx.respond(error)
+            await ctx.respond(error, delete_after=5)
         else:
             try:
-                await ctx.respond(error)
+                await ctx.respond(error, delete_after=5)
             except: pass
             raise error
 
